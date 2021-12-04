2150f8fa02344737347699774e1fbcd11e6113d6
Robert Vari <mail.pythonsuli@gmail.com>
2021-12-04 15:57:34 +0100

    update

--- 07_property.py
+++ 07_property.py
@@ -3,3 +3,27 @@ class Dice:
         # instance attributes
         self._color = color
         self._sides = sides
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, new_color):
+        self._color = new_color
+
+    @property
+    def sides(self):
+        return self._sides
+
+    @sides.setter
+    def sides(self, new_sides):
+        self._sides = new_sides
+
+
+my_dice = Dice("White", 6)
+print(my_dice.color)
+print(my_dice.sides)
+
+my_dice.color = "Red"
+my_dice.sides = 10
