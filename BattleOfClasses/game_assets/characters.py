46949bd55386152e361786144a00e7e436b5d358
Robert Vari <mail.pythonsuli@gmail.com>
2021-12-05 16:04:12 +0100

    Init project

--- BattleOfClasses/game_assets/characters.py
+++ BattleOfClasses/game_assets/characters.py
@@ -2,11 +2,19 @@ import random
 
 
 class CharacterBase:
+    races = {
+        "human": {"strength": 50, "max_HP": 100, "max_weight": 50},
+        "ork": {"strength": 130, "max_HP": 200, "max_weight": 100},
+        "elf": {"strength": 60, "max_HP": 100, "max_weight": 60},
+        "dwarf": {"strength": 130, "max_HP": 230, "max_weight": 150},
+    }
+
     def __init__(self):
         self.inventory = []
         self.max_weight = 0
         self.race = None
         self.golds = 0
+        self.name = None
 
         # combat stats
         self.strength = 0
@@ -14,7 +22,24 @@ class CharacterBase:
         self.current_HP = 0
 
     def create(self):
+        self.name = self.get_fantasy_name()
+        self.race = random.choice(list(self.races))
+        self.strength = self.races[self.race]["strength"]
+        self.max_HP = self.races[self.race]["max_HP"]
+        self.current_HP = self.max_HP
+        self.golds = random.randint(0, 100)
+        self.max_weight = self.races[self.race]["max_weight"]
+
         return self
+
+    def report(self):
+        print(f"Name: {self.name}")
+        print(f"Race: {self.race}")
+        print(f"Strength: {self.strength}")
+        print(f"Max HP: {self.max_HP}")
+        print(f"Max Weight: {self.max_weight}")
+        print(f"Golds: {self.golds}")
+        print(f"Inventory: {self.inventory}")
 
     @staticmethod
     def get_fantasy_name():
@@ -46,6 +71,5 @@ class NPC(CharacterBase):
 
 
 if __name__ == '__main__':
-
-    for _ in range(10):
-        print(Enemy.get_fantasy_name())
+    enemy = Enemy().create()
+    enemy.report()
