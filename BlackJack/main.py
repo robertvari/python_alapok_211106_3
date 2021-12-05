1423b955e78a56846ad592bf5d5c9755cc98e140
Robert Vari <mail.pythonsuli@gmail.com>
2021-12-05 14:32:33 +0100

    update

--- BlackJack/main.py
+++ BlackJack/main.py
@@ -1,6 +1,37 @@
 class Blackjack:
     def __init__(self):
         self._intro()
+        self.player_list = []
+
+        self.create_players()
+
+    def start_game(self):
+        # create deck
+        # store bet = 0
+
+        # for player in player_list
+           # ...
+
+        # decide who won the round
+        # give the bet to the winner
+
+        # if player has credits
+        #    ask of he/she want to play
+        # else
+        #   self.exit_game()
+        pass
+
+    def create_players(self):
+        # create AI players
+        # create player
+
+        # self.start_game()
+        pass
+
+    def exit_game(self):
+        print("Thank you for playing my game. Have a nice day! :)")
+        exit()
+        pass
 
     def _intro(self):
         print("*"*50, "Blackjack", "*"*50)
