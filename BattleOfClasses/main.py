554b3cbe0cdcce659cc29f28ffd855c90b95d850
Robert Vari <mail.pythonsuli@gmail.com>
2021-12-05 15:31:23 +0100

    Init project

---
+++ BattleOfClasses/main.py
@@ -0,0 +1,11 @@
+class BattleOfClasses:
+    def __init__(self):
+        self.intro()
+
+    @staticmethod
+    def intro():
+        print("*"*50, "BATTLE OF CLASSES", "*"*50)
+
+
+if __name__ == '__main__':
+    BattleOfClasses()
